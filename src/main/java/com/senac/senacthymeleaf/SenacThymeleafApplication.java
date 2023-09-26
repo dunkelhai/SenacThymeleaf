efab4426c31f28ac6164c119dc13b2d214072ea7
Alysson Oliveira <3oalysson@gmail.com>
2023-09-26 18:58:18 -0300

    Inicial

---
+++ src/main/java/com/senac/senacthymeleaf/SenacThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.senac.senacthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenacThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenacThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senac/senacthymeleaf/SenacThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.senacthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenacThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
