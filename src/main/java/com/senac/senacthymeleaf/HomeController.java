75c8d99432b50b71e98b9f0412c0d3d763dedd70
Alysson Oliveira <3oalysson@gmail.com>
2023-10-02 18:25:28 -0300

    Ajustes novos
    Ajustes novos

--- src/main/java/com/senac/senacthymeleaf/HomeController.java
+++ src/main/java/com/senac/senacthymeleaf/HomeController.java
@@ -3,6 +3,7 @@ package com.senac.senacthymeleaf;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,15 +11,19 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    private List<Pessoa> pessoas = new ArrayList<>();
+
     @GetMapping("/")
     public String home(Model model) {
-        List<Pessoa> pessoas = new ArrayList<>();
-        pessoas.add(new Pessoa("Alice"));
-        pessoas.add(new Pessoa("Bob"));
-        pessoas.add(new Pessoa("Carol"));
-
         model.addAttribute("pessoas", pessoas);
-
         return "index";
+    }
+
+    @PostMapping("/adicionar")
+    public String adicionarPessoa(String nome) {
+        if (nome != null && !nome.isEmpty()) {
+            pessoas.add(new Pessoa(nome));
+        }
+        return "redirect:/";
     }
 }
