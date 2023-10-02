50d2a9df8a2a15de36f475770b42a98bb3556dbc
Alysson Oliveira <3oalysson@gmail.com>
2023-10-02 18:18:30 -0300

    Update HomeController.java

--- src/main/java/com/senac/senacthymeleaf/HomeController.java
+++ src/main/java/com/senac/senacthymeleaf/HomeController.java
@@ -6,23 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        Scanner entrada = new Scanner(System.in);
         List<Pessoa> pessoas = new ArrayList<>();
-        boolean continuar = false;
-        do {
-            System.out.println("Digite um nome:");
-            String nome = entrada.nextLine();
-            pessoas.add(new Pessoa(nome));
-            System.out.println("Continuar?");
-            continuar = entrada.nextBoolean();
-        } while(continuar);
+        pessoas.add(new Pessoa("Alice"));
+        pessoas.add(new Pessoa("Bob"));
+        pessoas.add(new Pessoa("Carol"));
 
         model.addAttribute("pessoas", pessoas);
 
